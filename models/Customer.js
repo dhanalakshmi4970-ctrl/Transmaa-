6a34e41cabf7eb0cbc5e84b3c539cb704a4ab6af
Sreenanda <sreenandak017@gmail.com>
2026-09-01 21:00:40 +0530

    Initial backend setup

---
+++ models/Customer.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+
+const customerSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  phone: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  email: {
+    type: String
+  }
+});
+
+const Customer = mongoose.model("Customer", customerSchema);
+
+module.exports = Customer;
